9f4dce8e7c1d8dbc032640b6a9150898fed233f9
beaseobar <beaseobar@gmail.com>
2024-10-16 18:50:46 +0200

    added pseudocount

--- rbm/binary/grad.py
+++ rbm/binary/grad.py
@@ -6,6 +6,7 @@ def compute_gradient(
     data: Dict[str, torch.Tensor],
     chains: Dict[str, torch.Tensor],
     centered: bool = True,
+    pseudocount: bool= False,
 ) -> Dict[str, torch.Tensor]:
     """Computes the gradient of the log-likelihood. By default implements the centered version of the gradient,
     which normally improveds the quality of the learning.
@@ -20,9 +21,21 @@ def compute_gradient(
         Dict[str, torch.Tensor]: Gradient of the log-likelihood.
     """
     num_chains = len(chains["v"])
+
+    if pseudocount:
+        device=data["v"].device
+        old_values=data["v"].clone() # to perturbe for the pseudocount
+        Neff=data["weights"].sum()
+        random_probs = torch.rand(data["v"].shape,device=device)
+        change_mask = random_probs < 1./Neff/num_colors
+        new_values = torch.randint(0, num_colors, data["v"].shape,device=device)
+
+        data_new = torch.where(change_mask,new_values,old_values)
+    else:
+        data_new=data["v"].clone()
     
     # Averages over data and generated samples
-    v_data_mean = (data["v"] * data["weights"]).sum(0) / data["weights"].sum()
+    v_data_mean = (data_new * data["weights"]).sum(0) / data["weights"].sum()
     torch.clamp_(v_data_mean, min=1e-4, max=(1. - 1e-4))
     h_data_mean = (data["mh"] * data["weights"]).sum(0) / data["weights"].sum()
     v_gen_mean = chains["v"].mean(0)
@@ -32,7 +45,7 @@ def compute_gradient(
     
     if centered:
         # Centered variables
-        v_data_centered = data["v"]- v_data_mean
+        v_data_centered =data_new- v_data_mean
         h_data_centered = data["mh"] - h_data_mean
         v_gen_centered = chains["v"] - v_data_mean
         h_gen_centered = chains["h"] - h_data_mean
